98fd649e369cb9aba02fa3096679612240038e6b
Bharadwaj Kalicheti <bharadwajreddy1999@gmail.com>
2024-03-12 04:04:32 -0400

    Added some text in Home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -23,10 +23,8 @@ const Home = () => {
       <Header></Header>
       <h1>Welcome to Our Application!</h1>
       <p>This is the home page. Click on Map button on the header to View the dashboard</p>
-      <p>80 percent of features are inter active </p>
-      <p>Still need to add CSS for every page except the Map</p> 
-      <p>For map need to add Crime Details Page,University,crime and time selection </p>
-      <p>Once those are done CSS will be started for remaining pages</p>
+      <p>90 percent of features are inter active only the front end part still back end integartion is pending</p>
+      <p>Still need to add CSS for every page except the Map page</p> 
       <p>Then next steps to add is sign in functionality and dashboard for Admin user</p>
       <UniversityDropdown value={university} onChange={handleUniversityChange} />
       <GoButton onClick={handleGoClick} />
